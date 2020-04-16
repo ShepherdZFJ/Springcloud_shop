2f15c76fe86d411da7356c2d3adfbcabc89719d7
shepherd <zhengfj@isyscore.com>
2020-04-16 18:42:51 +0800

    实现获取商品信息功能

--- springcloud-product/src/main/java/com/shepherd/springcloudproduct/api/controller/ProductController.java
+++ springcloud-product/src/main/java/com/shepherd/springcloudproduct/api/controller/ProductController.java
@@ -1,6 +1,13 @@
 package com.shepherd.springcloudproduct.api.controller;
 
+import com.shepherd.springcloudproduct.api.service.ProductService;
+import com.shepherd.springcloudproduct.api.vo.ProductVO;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author fjZheng
@@ -8,5 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/4/15 19:37
  */
 @RestController
+@Api("应用配置中心")
+@RequestMapping("${spring.web.base-path}/product")
 public class ProductController {
+    @Resource
+    private ProductService productService;
+
+    @GetMapping()
+    public ProductVO getProductList(){
+        return productService.getProduct();
+    }
 }
